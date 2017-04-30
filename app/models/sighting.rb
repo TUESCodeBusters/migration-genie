9fb049108766aa370306a55b5e8141ffaa08d7c8
Dimitar Nikolov <dimitar@doonloo.com>
2017-04-30 03:54:35 +0300

    Finish relation sighting->species

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -5,4 +5,8 @@ class Sighting < ApplicationRecord
     accepts_nested_attributes_for :species
     accepts_nested_attributes_for :user
     mount_uploader :photo, SightingPhotoUploader
+
+    def species_attributes=(attributes)
+        self.species = Species.where(attributes).first_or_create
+    end
 end
